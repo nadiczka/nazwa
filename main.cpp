08eacbf2c61d1626a6425945f3fcd3baf7936df3
nadiczka <jamroziknadia@gmail.com>
2019-12-18 16:28:15 +0100

    tak

--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
-
+#include <list>
 
 enum class PackageQueueType {
     FIFO, LIFO
 };
 
+using ElementID = int;
 
 class PackageQueue {
 public:
-    PackageQueue(PackageQueueType);
+    PackageQueue(PackageQueueType queue): {
+        switch (queue) {
+            case PackageQueueType::FIFO:
+                //tu nizej jakas instukcja
+                std::cout << "stos" << std::endl;
+                break;
+            case PackageQueueType::LIFO:
+                //tu tez
+                std::cout << "kolejka" << std::endl;
+                break;
+        }
+    }
+
+
+    PackageQueue emplace_back() { return PackageQueue.emplace_back() } //daje element na koniec listy
+
+private:
+    std::list<ElementID> QueueType;
 };
 
 int main() {
