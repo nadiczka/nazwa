9280a03b021ad4151268a3f3eb5704e9675cf112
nadiczka <jamroziknadia@gmail.com>
2019-12-18 14:08:19 +0100

    tak

--- src/package.cpp
+++ src/package.cpp
@@ -2,7 +2,7 @@
 // Created by igach on 18.12.2019.
 //
 
-#include "../src/package.hpp"
+#include "../include/package.hpp"
 
 std::set<ElementID> Package::assigned_IDs;
 std::set<ElementID> Package::freed_IDs;
