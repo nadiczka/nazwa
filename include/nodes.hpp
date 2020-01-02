7e648e3414abae9d47dd7d5ba093f469fb7a0909
Wojtek <janhunyady@gmail.com>
2020-01-02 12:40:49 +0100

    test wojtek5

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -15,8 +15,19 @@ public:
     void add_receiver(IPackageReceiver* receiver);
     void remove_receiver(IPackageReceiver* receiver);
 private:
-    std::map<IPackageReceiver*,double> _preferences;
+    std::map<IPackageReceiver*,double> preferences_;
     double temporary_probability;
+};
+
+class IPackageReceiver {
+public:
+    IPackageReceiver(ReceiverPreferences receiver_preferences);
+    void send_package();
+    std::optional<Package> get_sending_buffer();
+protected:
+    void push_package(Package&& package);
+private:
+    ReceiverPreferences receiver_preferences_;
 };
 
 #endif //SIMNET_NODES_HPP
--- src/nodes.cpp
+++ src/nodes.cpp
@@ -9,24 +9,32 @@ ReceiverPreferences::ReceiverPreferences(std::function<double()> rand_function =
 }
 
 ReceiverPreferences::add_receiver(IPackageReceiver* receiver){
-    _preferences.insert(receiver, temporary_probability);
+    preferences_.insert(receiver, temporary_probability);
     temporary_probability = 0;
 }
 
 ReceiverPreferences::remove_receiver(IPackageReceiver* receiver) {
-    _preferences.erase(receiver);
+    preferences_.erase(receiver);
 }
 
 ReceiverPreferences::choose_receiver() {
     maximum = 0;
-    for (auto& i : _preferences.items()){
+    for (auto& i : preferences_.items()){
         if (i->second > maximum) {
             maximum = i->second
         }
     }
-    for (auto& i : _preferences.items()){
+    for (auto& i : preferences_.items()){
         if (i->second = maximum) {
             return i->first;
         }
     }
 }
+
+IPackageReceiver::IPackageReceiver(ReceiverPreferences receiver_preferences){
+    receiver_preferences_ = receiver_preferences;
+}
+
+IPackageReceiver::send_package{
+    //I DONT KNOW... YET
+};
